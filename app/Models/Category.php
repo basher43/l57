b7135700ed3298d3f8ff76e5d97ad6e47fda9bc1
basher43 <md.abulbasher43@gmail.com>
2020-02-17 11:43:32 +0600

    Category model added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public $guarded = [];
+
+    public function parent_category(){
+        return $this->belongsTo(__CLASS__);
+    }
+    public function child_category(){
+        return $this->hasMany(__CLASS__);
+    }
 }
