915da333b9f2a3f006b46a84e45a8433114abeb9
basher43 <md.abulbasher43@gmail.com>
2020-02-18 13:16:36 +0600

    Home page setup done

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function showHomePage()
+    {
+        $data = [];
+        $data['products'] = Product::paginate(9);
+        return view('frontend.home', $data);
+    }
+}
---
+++ resources/views/frontend/partials/_hero.blade.php
@@ -0,0 +1,10 @@
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1>{{config('app.name')}}</h1>
+        <p class="lead text-muted">Basher43 ecommerce Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequatur deleniti deserunt eum ipsam obcaecati possimus provident suscipit tempora, temporibus!</p>
+        <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        </p>
+    </div>
+</section>
